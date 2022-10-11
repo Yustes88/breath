c8901bbbcae23214dbf3da4c64dfd4553e9bc0d1
Yulya <iuliiaobr.io@gmail.com>
2022-10-11 10:14:55 +0700

    Adds grow and shrink animation

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const text = document.querySelector('.text')
+const container = document.querySelector('.container')
 const totalTime = 16000;
 const period = totalTime / 4;
 
 const startGame = () => {
     text.textContent = 'Breath in';
-
+    container.className = 'container grow'
     setTimeout(() => {
         text.textContent = 'Hold';
 
         setTimeout(() => {
         text.textContent = 'Breath out';
-
+        container.className = 'container shrink'
         setTimeout(() => {
             text.textContent = 'Hold';
         }, period)
